fd35877da021e225ee97d33349868688cd52b02f
Stevenuns <rflsilvabenitez@gmail.com>
2023-03-07 21:08:11 -0400

    agregue ejercicio 1.py

---
+++ ejercicio 1.py
@@ -0,0 +1,8 @@
+numeroIf = -5  # puedes cambiar el valor de la variable numeroIf
+
+if numeroIf > 0:
+    print("Es un número positivo")
+elif numeroIf == 0:
+    print("Es cero")
+else:
+    print("Es un número negativo")
