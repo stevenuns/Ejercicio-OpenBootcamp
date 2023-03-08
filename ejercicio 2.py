eefd28ef527ff183cf356527f7fbf1c446294ff2
Stevenuns <rflsilvabenitez@gmail.com>
2023-03-07 21:48:30 -0400

    agregue ejercicio 3.py

--- ejercicio 2.py
+++ ejercicio 2.py
@@ -3,4 +3,4 @@ numeroWhile = 0
 while numeroWhile < 3:
     numeroWhile += 1
     print(numeroWhile)
-    
+
---
+++ ejercicio 3.py
@@ -0,0 +1,10 @@
+# Definimos una variable de control
+ejecutar = True
+
+# Bucle Do While
+while ejecutar:
+    # Aquí iría el código que queremos que se ejecute al menos una vez
+    print("Este código se ejecuta al menos una vez")
+    
+    # Modificamos la variable de control para salir del bucle
+    ejecutar = False
