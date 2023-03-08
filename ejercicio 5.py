3bb10cfb29449a2efa7a75c3f66f2764c51c4d88
Stevenuns <rflsilvabenitez@gmail.com>
2023-03-07 21:58:39 -0400

    agregue ejercicio 5.py

---
+++ ejercicio 5.py
@@ -0,0 +1,12 @@
+estacion = "invierno"
+
+if estacion == "verano":
+    print("Estamos en verano")
+elif estacion == "otoño":
+    print("Estamos en otoño")
+elif estacion == "invierno":
+    print("Estamos en invierno")
+elif estacion == "primavera":
+    print("Estamos en primavera")
+else:
+    print("La estación ingresada no es válida")
